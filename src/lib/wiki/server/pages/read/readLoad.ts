9cc9a9ea8024e9a36d5a0b2c81087d8d9f876fa7
nemoVim <nemovim@kentech.ac.kr>
2025-07-21 00:56:58 +0900

    style: Update the read page

--- src/lib/wiki/server/pages/read/readLoad.ts
+++ src/lib/wiki/server/pages/read/readLoad.ts
@@ -2,7 +2,7 @@ import { redirect, type ServerLoadEvent } from '@sveltejs/kit';
 
 import type { WikiResponse } from '@nemowiki/core/types';
 import { getRedirectFullTitleByMarkup, readDocByFullTitle } from '@nemowiki/core';
-import { decodeFullTitle, encodeFullTitle } from '@nemowiki/core/client';
+import { encodeFullTitle } from '@nemowiki/core/client';
 
 import modifyHtmlByExistenceOfLinks from '$lib/wiki/utils/modifyHtml.js';
 
@@ -10,7 +10,7 @@ export async function readLoad({
 	params,
 	locals,
 	url
-}: ServerLoadEvent): Promise<WikiResponse<{ doc: string; rev: number; from: string | null }>> {
+}: ServerLoadEvent): Promise<WikiResponse<{ doc: string }>> {
 	const fullTitle = params.fullTitle;
 	if (!fullTitle)
 		return {
@@ -18,8 +18,7 @@ export async function readLoad({
 			reason: 'fullTitle is undefined'
 		};
 
-	let rev = Number(url.searchParams.get('rev'));
-	if (rev === 0) rev = -1;
+	const rev = Number(url.searchParams.get('rev')||'-1');
 
 	const res_read = await readDocByFullTitle(fullTitle, locals.user, rev);
 	if (!res_read.ok) return res_read;
@@ -35,10 +34,6 @@ export async function readLoad({
 
 	return {
 		ok: true,
-		value: {
-			rev,
-			from: decodeFullTitle(url.searchParams.get('from') || '') || null,
-			doc: JSON.stringify(res_read.value)
-		}
+		value: { doc: JSON.stringify(res_read.value) }
 	};
 }
--- src/routes/r/[...fullTitle]/+page.svelte
+++ src/routes/r/[...fullTitle]/+page.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
 	import type { Doc } from '@nemowiki/core/types';
+	import { page } from '$app/state';
 
+	import CommonWarn from '$lib/wiki/components/common/commonWarn.svelte';
+	import CommonInfo from '$lib/wiki/components/common/commonInfo.svelte';
 	import ReadHeader from '$lib/wiki/components/pages/read/readHeader.svelte';
-	import ReadCaution from '$lib/wiki/components/pages/read/readCaution.svelte';
 	import HtmlContent from '$lib/wiki/components/common/htmlContent.svelte';
 
 	let { data } = $props();
 
-	let rev = $derived<number>(data?.value?.rev as number);
-	let from = $derived<string | null>(data?.value?.from || null);
+	let rev = $derived<number | null>(Number(page.url.searchParams.get('rev')||'-1') || -1);
+	let from = $derived<string | null>(page.url.searchParams.get('from'));
 	let doc = $derived<Doc | null>(JSON.parse(data?.value?.doc || 'null'));
 
 	let html = $derived<string>(doc?.html || '');
@@ -16,10 +18,15 @@
 
 <article>
 	<ReadHeader revision={doc?.revision || 0} />
+	{#if from}
+		<CommonInfo><a href="/r/{from}?redirect=no">{from}</a>에서 넘어옴.</CommonInfo>
+	{/if}
 	{#if !data.ok}
 		<p>{data.reason}</p>
 	{:else}
-		<ReadCaution ok={data.ok} {rev} {from} />
+		{#if rev !== -1}
+			<CommonWarn>이 문서는 <b>{rev}</b>번째 수정판임에 유의하세요.</CommonWarn>
+		{/if}
 		<HtmlContent content={html} />
 	{/if}
 </article>
